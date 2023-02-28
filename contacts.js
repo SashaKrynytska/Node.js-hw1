b76491ec623683c0ddd50a1efa63d3c4034dbc7e
SashaKrynytska <krynytskaya@gmail.com>
2023-02-28 10:41:40 +0200

    update

--- contacts.js
+++ contacts.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 const path = require("path");
 const crypto = require("crypto");
 
-const contactsPath = path.resolve(__dirname, ".", "db", "contacts.json");
+const contactsPath = path.resolve(__dirname, "db", "contacts.json");
 
 const listContacts = async () => {
   try {
